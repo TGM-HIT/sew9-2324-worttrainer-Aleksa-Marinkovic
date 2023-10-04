00c2a985f6366c5fa16af896a337e0e02acb60a9
Aleksa Marinkovic <aleksa13.am@gmail.com>
2023-10-04 09:00:41 +0200

    Wortpaare Klasse fertig

---
+++ src/main/java/amarinkovic/Wortpaare.java
@@ -0,0 +1,33 @@
+package amarinkovic;
+
+
+public class Wortpaare {
+    private String wort;
+    private String bildURL;
+
+    public Wortpaare(String wort, String bildURL) {
+        this.wort = wort;
+        this.bildURL = bildURL;
+    }
+
+    public String getWort() {
+        return wort;
+    }
+
+    public String getBildURL() {
+        return bildURL;
+    }
+
+    public void setWort(String wort) {
+        if(!bildURL.endsWith(".png") && !bildURL.endsWith(".jpg") && !bildURL.endsWith(".jpeg")) {
+            throw new IllegalArgumentException("URL is not an image!");
+        }
+    }
+
+    public void setBildURL(String bildURL) {
+        this.bildURL = bildURL;
+    }
+    public String toString() {
+        return this.wort + ":" + this.bildURL;
+    }
+}
